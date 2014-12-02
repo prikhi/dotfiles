95e62c88f6dd004ee070558491f76cd618aaa130
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2014-12-02 14:54:23 -0500

    Migrate to dotbot for installation and configuration

--- install.sh
+++ install.sh
@@ -1,41 +1,13 @@
 #!/usr/bin/env bash
-function link_file {
-    source="${PWD}/$1"
-    target="${HOME}/${1/_/.}"
+set -e
 
-    if [ -e "${target}" ] && [ ! -L "${target}" ]; then
-        mv $target $target.df.bak
-    fi
+CONFIG="config.yaml"
+DOTBOT_DIR="dotbot/dotbot"
 
-    ln -sf ${source} ${target}
-}
+DOTBOT_BIN="dotbot/bin/dotbot"
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-function unlink_file {
-    source="${PWD}/$1"
-    target="${HOME}/${1/_/.}"
+cd "${BASEDIR}"
+git submodule update --init --recursive "${DOTBOT_DIR}"
 
-    if [ -e "${target}.df.bak" ] && [ -L "${target}" ]; then
-        unlink ${target}
-        mv $target.df.bak $target
-    fi
-}
-
-if [ "$1" = "vim" ]; then
-    for i in _vim*
-    do
-       link_file $i
-    done
-elif [ "$1" = "restore" ]; then
-    for i in _*
-    do
-        unlink_file $i
-    done
-    exit
-else
-    for i in _*
-    do
-        link_file $i
-    done
-fi
-
-git submodule update --init --recursive
+"${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
